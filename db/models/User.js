06513081197d01ebead472afdf4641b266dda8e9
ALAN TONELLO <alantb2@gmail.com>
2024-02-15 17:10:08 +0100

    implemented models and basic database structure

--- db/models/User.js
+++ db/models/User.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
+  passwordHash: { type: String, required: true }, // Hashing functions are one-way and transform a password into an  irreversible string (the hash). You store the hash and not the original password itself. When verifying a user login, you hash the provided password and compare it to the stored hash. If they match, the password was correct.
   buildingAddress: { type: String, required: true },
 });
 
