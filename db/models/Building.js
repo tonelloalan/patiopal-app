39df411683e8755d5e5a69578c2b5658dbb09016
ALAN TONELLO <alantb2@gmail.com>
2024-02-15 16:38:49 +0100

    update comment to building model

--- db/models/Building.js
+++ db/models/Building.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const buildingSchema = new Schema({
   address: { type: String, required: true, unique: true },
-  // The reference below will allow to link individual users to a building easily. By storing resident ObjectIds, we say "these specific users are associated with this building."
+  // The reference below will allow to link individual users to a building easily. The buildingSchema has a residents field designed as an array of ObjectIds. An ObjectId is a unique identifier that MongoDB assigns to every document.  By storing resident ObjectIds,  we say "these specific users are associated with this building."
   residents: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
