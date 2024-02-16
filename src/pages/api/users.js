4bfef54dad178463fd14b116d0ae6b3dc03cd1da
ALAN TONELLO <alantb2@gmail.com>
2024-02-16 12:16:18 +0100

    finish basic users api route setup

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     return res.status(200).json(users);
   }
   if (req.method === "POST") {
-    console.log("request: ", req.body);
+    // console.log("request: ", req.body);
     try {
       const userData = req.body;
       await User.create(userData);
