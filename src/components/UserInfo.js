fbcc31bd1b0d968288aba9e5d535039cb8ff01f5
ALAN TONELLO <alantb2@gmail.com>
2024-02-17 15:17:25 +0100

    finished register and login frontend

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,3 +1,17 @@
+import styles from "@/styles/UserInfo.module.css";
+
 export default function UserInfo() {
-  return <div>User Info</div>;
+  return (
+    <div className={styles.userInfoContainer}>
+      <div className={styles.userInfo}>
+        <div>
+          Name: <span>Alan Tonello</span>
+        </div>
+        <div>
+          Email: <span>alantb2@gmail.com</span>
+        </div>
+        <button>Log out</button>
+      </div>
+    </div>
+  );
 }
