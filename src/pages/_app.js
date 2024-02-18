d0bdfc14d3885849a5adfb2712926a609bc0b87d
ALAN TONELLO <alantb2@gmail.com>
2024-02-18 23:16:22 +0100

    moved Providers to outside pages folder to prevent building error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 // Wrapping the Session Provider here (_app.js) ensures session data is available throughout, since Components within here get rendered for every page of the application,
 
 import "@/styles/globals.css";
-import { AuthProvider } from "@/pages/Providers";
+import { AuthProvider } from "@/Providers";
 
 export default function App({ Component, pageProps }) {
   return (
