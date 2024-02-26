2bbd1b722c154a93a98d5fa89e7a20f645a06cfa
ALAN TONELLO <alantb2@gmail.com>
2024-02-26 16:26:32 +0100

    small sample data ui update

--- src/pages/buildings/[buildingId]/messageBoard.js
+++ src/pages/buildings/[buildingId]/messageBoard.js
@@ -4,59 +4,98 @@ export default function MessageBoard() {
       <h3>Hello! This is a Message Board</h3>
 
       <div style={{ textAlign: "left" }}>
-        <span style={{ fontSize: "x-small" }}>
-          <p>A. Tonello, on 26.02.2023 15:54</p>
+        <span style={{ fontSize: "x-small", padding: "0", margin: "0" }}>
+          <p>
+            <span style={{ fontWeight: "bold" }}>A. Tonello</span>, on
+            26.02.2023 15:54
+          </p>
         </span>
         <p
           style={{
             backgroundColor: "white",
             borderRadius: "13px",
             padding: "8px",
+            margin: "0",
+            marginBottom: "25px",
           }}
         >
           This is a sample message.
         </p>
       </div>
       <div style={{ textAlign: "right" }}>
-        <span style={{ fontSize: "x-small" }}>
-          <p>C. Amend, on 26.02.2023 15:57</p>
+        <span style={{ fontSize: "x-small", padding: "0", margin: "0" }}>
+          <p>
+            <span style={{ fontWeight: "bold" }}>C. Amend</span>, on 26.02.2023
+            15:57
+          </p>
         </span>
         <p
           style={{
             backgroundColor: "#B0B6AC",
             borderRadius: "13px",
             padding: "8px",
+            margin: "0",
+            marginBottom: "25px",
           }}
         >
           This is another sample message. 🙋
         </p>
       </div>
       <div style={{ textAlign: "left" }}>
-        <span style={{ fontSize: "x-small" }}>
-          <p>A. Tonello, on 26.02.2023 16:00</p>
+        <span style={{ fontSize: "x-small", padding: "0", margin: "0" }}>
+          <p>
+            <span style={{ fontWeight: "bold" }}>A. Tonello</span>, on
+            26.02.2023 16:00
+          </p>
         </span>
         <p
           style={{
             backgroundColor: "white",
             borderRadius: "13px",
             padding: "8px",
+            margin: "0",
+            marginBottom: "25px",
           }}
         >
           This is a sample message yet again. 👋
         </p>
       </div>
       <div style={{ textAlign: "left" }}>
-        <span style={{ fontSize: "x-small" }}>
-          <p>J. Schneider, on 26.02.2023 16:04</p>
+        <span style={{ fontSize: "x-small", padding: "0", margin: "0" }}>
+          <p>
+            <span style={{ fontWeight: "bold" }}>J. Schneider</span>, on
+            26.02.2023 16:04
+          </p>
         </span>
         <p
           style={{
             backgroundColor: "white",
             borderRadius: "13px",
             padding: "8px",
+            margin: "0",
+            marginBottom: "25px",
           }}
         >
           This is a sample message, so many 😅
+        </p>
+      </div>
+      <div style={{ textAlign: "right" }}>
+        <span style={{ fontSize: "x-small", padding: "0", margin: "0" }}>
+          <p>
+            <span style={{ fontWeight: "bold" }}>C. Amend</span>, on 26.02.2023
+            16:13
+          </p>
+        </span>
+        <p
+          style={{
+            backgroundColor: "#B0B6AC",
+            borderRadius: "13px",
+            padding: "8px",
+            margin: "0",
+            marginBottom: "25px",
+          }}
+        >
+          Cool chat, guys. 👏
         </p>
       </div>
       <input placeholder="Type your message here..." />
